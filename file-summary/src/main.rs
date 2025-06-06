d4a2edd07106834a8969994bf66fe5417b4d1306
Sean Murdoch <145845458+Wytchwulf@users.noreply.github.com>
2025-06-06 14:21:29 +0000

    Added comments for clarity and reference

--- file-summary/src/main.rs
+++ file-summary/src/main.rs
@@ -18,8 +18,10 @@ fn main() {
         return;
     };
 
+    // Create path from argument
     let path = PathBuf::from(&args[1]);
 
+    // Populate hashmap from recursive file walk
     let map = match recursive(&path) {
         Ok(result) => result,
         Err(e) => {
@@ -28,35 +30,57 @@ fn main() {
         }
     };
 
+    // Convert HashMap to List
     let mut summary: Vec<_> = map.iter().collect();
+    // Sort list by file size -- Compare b with &a for Descending 
     summary.sort_by(|a, b| b.1.0.cmp(&a.1.0));
 
+    // Print data for each entry in list
     for (ext, (size, count)) in summary {
-        println!("{:<8} {:>6} files  {:>10}", ext, count, format_size(*size));
+        // {:<10} → left-align ext to 10 chars
+        // {:>6}  → right-align count to 6 chars
+        // {:>10} → right-align formatted size to 10 chars
+        // *size  → dereference &u64 to pass u64 to format_size()
+        println!("{:<10} {:>6} files  {:>10}", ext, count, format_size(*size));
     }
 }
 
 fn recursive(path: &Path) -> Result<HashMap<String, (u64, usize)>, std::io::Error> {
+    // Access metadata of path
     let meta = metadata(path)?;
+    // Create an empty HashMap to store (ext → (total size, count))
     let mut result: HashMap<String, (u64, usize)> = HashMap::new();
 
+    // If path lands on a file
     if meta.is_file() {
+        // Extract size of file in bytes
         let size = meta.len();
+
         let ext = path
+            // Get extension
             .extension()
+            // Convert OsStr to Str
             .and_then(|e| e.to_str())
+            // Convert extension to lowercase
             .map(|s| s.to_lowercase())
+            // Use extension if it exists; otherwise return "(none)" using a closure 
             .unwrap_or_else(|| "(none)".to_string());
 
+        // Give me the value for ext, or insert (0, 0) if it’s not there — and give me a mutable reference to it.
         let entry = result.entry(ext).or_insert((0, 0));
         entry.0 += size;
         entry.1 += 1;
     };
 
+    // If path lands on a directory
     if meta.is_dir() {
+        // Read the contents of the directory and for each:
         for entry in fs::read_dir(path)? {
+            // Give me the entry, not the errors
             let entry = entry?;
+            // Create a hashmap and populate it with recursion
             let children = recursive(&entry.path())?;
+            // Merge child summary into the current result map
             for (ext, (size, count)) in children {
                 let entry = result.entry(ext).or_insert((0, 0));
                 entry.0 += size;
